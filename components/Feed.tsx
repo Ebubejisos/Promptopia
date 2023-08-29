5897c762a0a1fde4bf47dce4a006f4bad6c5d2eb
Ebubejisos <anosikegideon007@gmail.com>
2023-08-29 06:50:21 +0100

    update!!

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+'use client';
+
+import React, {
+  useState,
+  useEffect,
+  ChangeEventHandler,
+  MouseEventHandler,
+} from 'react';
+import PromptCard from './PromptCard';
+
+interface Posts {
+  _id: string;
+  creator: any;
+  prompt: string;
+  tag: string;
+}
+
+interface PromptCardListPropType {
+  data: Posts[];
+  handleTagClick: MouseEventHandler;
+}
+
+const PromptCardList = ({ data, handleTagClick }: PromptCardListPropType) => {
+  return (
+    <div className='prompt_layout mt-16'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState<Posts[]>([]);
+
+  const handleSearchChange: ChangeEventHandler = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='flex-center relative w-full'>
+        <input
+          type='text'
+          placeholder='Search tags or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
